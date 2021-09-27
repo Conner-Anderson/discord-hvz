f9ba58006af2a4fb217ecabc8e8548198073b604
ConnerHAnderson <connerhanderson@gmail.com>
2021-09-26 22:32:19 -0500

    Preparing a revoke command. Misc.

--- discord-hvz.py
+++ discord-hvz.py
@@ -550,6 +550,48 @@ async def tag_delete(ctx, tag_id: int):
         sheets.export_to_sheet('tags')
 
 '''
+@tag.command(name='revoke')
+@commands.has_role('Admin')
+@check_event
+async def tag_revoke(ctx, tag_id: int):
+    
+    Removes the tag by its ID, reverting tagged member to human.
+
+    Takes a tag ID, which you can get from the Google sheet.
+    Removes the tag from the database. Also changes the tagged member back to
+    human if there aren't any remaining tags on them.
+    
+    try:
+        tag_row = db.get_tag(tag_id)
+        if tag_row is None:
+            await ctx.message.reply(f'Could not find a tag with ID \"{tag_id}\"')
+            return
+        db.edit
+        sheets.export_to_sheet('tags')
+        msg = ''
+
+        tagged_member = bot.guild.get_member(int(tag_row.Tagged_ID))
+        existing_tag = db.get_tag(tag_row.Tagged_ID, column='Tagged_ID')
+        if existing_tag is None:
+            # Change to human if there are no previous tags on the tagged member
+            
+            # db.edit_member(tagged_member, 'Faction', 'human')
+            await tagged_member.add_roles(bot.roles['human'])
+            await tagged_member.remove_roles(bot.roles['zombie'])
+            msg += f'Changed <@{tagged_member} to human.>'
+        else:
+            msg += f'Left <@{tagged_member}> as zombie because <@{existing_tag.Tagger_ID}> still tagged them. ' 
+            f'(Tag ID: {existing_tag.Tagger_ID}'
+
+    except Exception as e:
+        log.exception(e)
+        await ctx.send(f'Command error! Error: {e}')
+    else:
+        msg = f'Tag {tag_id} deleted. ' + msg
+        await ctx.message.reply(msg)
+        sheets.export_to_sheet('tags')
+
+
 @bot.group(name='config')
 @commands.has_role('Admin')
 @check_event
@@ -569,9 +611,9 @@ async def config_command(ctx, setting: str, choice: bool = None):
     If only 'setting' is provided, prints the current setting.
     If 'choice' is True or False, the config setting is set.
     Current 'setting' options:
-        'registration' Is the registration button enabled?
-        'tag_logging' Is the tag log button enabled?
-        'silent_oz' Are tag announcements made for OZ tags?
+        'registration' Is the registration button enabled? Default: True
+        'tag_logging' Is the tag log button enabled? Default: True
+        'silent_oz' Are OZ names omitted from tag announcements? Default: False
     '''
     if setting.casefold() not in ('registration', 'tag_logging', 'silent_oz'):
         await ctx.message.reply('Conner has not implemented full config access yet. Do !help config')
@@ -693,6 +735,7 @@ async def resolve_chat(chatbot):  # Called when a ChatBot returns 1, showing it 
             
             db.edit_member(tagged_member, 'Faction', 'zombie')
             sheets.export_to_sheet('members')
+
             msg = f'<@{tagged_member_id}> has turned zombie!'
             if not config['silent_oz']:
                 msg += f'\nTagged by <@{chatbot.target_member.id}>'
