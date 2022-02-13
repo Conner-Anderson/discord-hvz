6337374043fed061e5f9777a018fd49a577846e9
ConnerHAnderson <conneranderson.dev@gmail.com>
2022-02-13 11:27:04 -0600

    Restructure. Basic chatbot functionality, no saving yet.

--- chatbot.py
+++ chatbot.py
@@ -29,8 +29,14 @@ def dump(obj):
         print("obj.%s = %r" % (attr, getattr(obj, attr)))
 
 
+class ResponseError(ValueError):
+    def __init__(self, message=None):
+        if message is not None:
+            super().__init__(message)
+
+
 @dataclass(frozen=True)
-class Question:
+class QuestionData:
     name: str
     display_name: str
     query: str
@@ -42,24 +48,23 @@ class Question:
     ]  # Attributes where if one appears, the other must also
 
     @classmethod
-    def build(cls, question_data: Dict):
+    def build(cls, question_data: Dict) -> QuestionData:
         for pair in cls.coupled_attributes:  # Throw error if both of a pair of coupled attributes don't exist
             for i in range(0, 2):
                 this_attr = pair[i]
-                if question_data.get(this_attr) is not None:
-                    other_attr = pair[int(not i)]  # Invert
-                    if question_data.get(other_attr) is None:
-                        raise ConfigError(
-                            f'If a question has attribute {this_attr}, it must also have {other_attr}. Check scripts.yml')
+                other_attr = pair[int(not i)]  # Invert
+                if question_data.get(this_attr) is not None and question_data.get(other_attr) is None:
+                    raise ConfigError(
+                        f'If a question has attribute {this_attr}, it must also have {other_attr}. Check scripts.yml')
         try:
-            return Question(**question_data)
+            return QuestionData(**question_data)
         except TypeError as e:
             e_text = repr(e)
             name = question_data.get('name')
             if name is None:
                 name = ''
             if 'missing' in e_text:
-                attribute = e_text[e_text.find('\''):-2] # Pulls the attribute from the error message
+                attribute = e_text[e_text.find('\''):-2]  # Pulls the attribute from the error message
                 raise ConfigError(
                     f'Question {name} is missing the required attribute {attribute}. Check scripts.yml') from e
             elif 'unexpected' in e_text:
@@ -70,38 +75,31 @@ class Question:
                 raise e
 
 
-@dataclass
-class Responses:
-    kind: str
-    table: str
-    _questions: List[Question]
-
-    def __post_init__(self):
-        pass
-
-
 @dataclass(frozen=True)
-class ChatBotScript:
+class ScriptData:
     """
 
     """
     kind: str
     table: str
-    questions: List[Question]
+    questions: List[QuestionData]
     beginning: str = ''
     ending: str = ''
 
     @classmethod
-    def build(cls, kind: str, script: Dict) -> ChatBotScript:
+    def build(cls, kind: str, script: Dict) -> ScriptData:
+        if script.get('questions') is None:
+            raise ConfigError
         questions = []
         for q in script.pop('questions'):
-            questions.append(Question.build(q))
+            questions.append(QuestionData.build(q))
+
         try:
-            return ChatBotScript(kind=kind, questions=questions, **script)
+            return ScriptData(kind=kind, questions=questions, **script)
         except TypeError as e:
             e_text = repr(e)
             if 'missing' in e_text:
-                attribute = e_text[e_text.find('\''):-2] # Pulls the attribute from the error message
+                attribute = e_text[e_text.find('\''):-2]  # Pulls the attribute from the error message
                 raise ConfigError(
                     f'Script \'{kind}\' is missing the required attribute {attribute}. Check scripts.yml') from e
             elif 'unexpected' in e_text:
@@ -111,69 +109,120 @@ class ChatBotScript:
             else:
                 raise e
 
+    @property
+    def length(self):
+        return len(self.questions)
+
+    def get_question(self, question_number: int):
+        return self.questions[question_number]
+
+    def get_query(self, question_number: int):
+        return self.questions[question_number].query
+
+
+@dataclass()
+class Question:
+    pass
+
+
+@dataclass()
+class Script:
+    data: ScriptData
+    kind: str = field(init=False, default=None)
+    questions: Dict[int, QuestionData] = field(init=False, default_factory=dict)
+    responses: Dict[int, Union[str, None]] = field(init=False, default_factory=dict)
+    last_asked_question: int = field(init=False, default=0)
+    next_question: int = field(init=False, default=0)
+    reviewing: bool = field(init=False, default=False)
+    # next_review_question: int = field(init=False, default=0)
+
+    def __post_init__(self):
+        for i, q in enumerate(self.data.questions):
+            self.questions[i] = q
+            self.responses[i] = None
+        self.kind = self.data.kind
 
     @property
     def length(self):
-        return len(self._questions)
+        return len(self.questions)
 
     @property
     def review_string(self) -> str:
+        # Return a string list of questions and responses
         output = ''
-        for q in self._questions:  # Build a list of the questions and their responses
-            output += (q.display_name + ': ' + self.responses[q.name] + '\n')
+        for i, q in self.questions.items():
+            response = self.responses[i]
+            output += f"**{q.display_name}**: {response}\n"
         return output
 
-    def get_question(self, question_number: int):
-        return self._questions[question_number]
+    def ask_next_question(self, existing_script: Script = None, first=False) -> str:
+        this_question = self.next_question
+        if this_question >= self.length:
+            log.info('Entered reviewing mode')
+            self.reviewing = True
+            self.last_asked_question = self.length
+            return self.review_string
+
+        output = ''
+        if existing_script is not None:
+            output += f'Cancelled the previous {existing_script.kind} conversation.\n'
+        if first:
+            output += f'{self.data.beginning} \n\n'
+        output += self.questions[this_question].query
+
+        self.last_asked_question = this_question
+        return output
+
+    def receive_response(self, response: str) -> None:
+        if self.reviewing and self.last_asked_question >= self.length:
+            log.info('Processing review selection')
+            selection = response.casefold()
+            for i, q in self.questions.items():
+                if selection == (q.name.casefold() or q.display_name.casefold()):
+                    self.next_question = i
+                    return
+            else:
+                message = f"'{selection}' is not a valid option. Please try again.'"
+                raise ResponseError(message)
+
+        question = self.questions[self.last_asked_question]
+        if question.valid_regex is not None:
+            match = regex.fullmatch(r'{}'.format(question.valid_regex), response)
+            if match is None:
+                message = f'{question.rejection_response} Please try again.'
+                raise ResponseError(message)
+
+        self.responses[self.next_question] = response
+        if self.reviewing:
+            self.next_question = self.length
+        else:
+            self.next_question = self.last_asked_question + 1
 
 
 @dataclass
 class ChatBot:
-    script: ChatBotScript
+    script: Script
     database: HvzDb
     chat_member: discord.Member
     target_member: discord.Member = None
     processing: bool = field(default=False, init=False)
     reviewing: bool = field(default=False, init=False)
-    last_asked_question: int = field(default=0, init=False)
-    responses: dict[str, any] = field(default=None, init=False)
 
     def __post_init__(self, ):
-        self.responses = self.script.response_dict
-
         if self.target_member is None:
             self.target_member = self.chat_member
 
-    async def start(self, existing_chatbot_kind: str = None):
-        await self.ask_question(0, starting=True, existing_chatbot_kind=existing_chatbot_kind)
-
-    async def ask_question(self, question_number: int, starting: bool = False, existing_chatbot_kind: str = None):
-        question = self.script.get_question(question_number)
-        msg = ''
-        if existing_chatbot_kind is not None:
-            msg += f'Cancelled the previous {existing_chatbot_kind} conversation.\n'
-        if starting:
-            msg += (self.script.beginning + '\n\n')
-        msg += question.query
+    async def ask_question(self, existing_chatbot: ChatBot = None, first: bool = False):
+        msg = self.script.ask_next_question(existing_script=getattr(existing_chatbot, 'script', None), first=first)
         await self.chat_member.send(msg)
-        self.last_asked_question = question_number
 
     async def receive(self, message: discord.Message):
-        question = self.script.get_question(self.last_asked_question)
-        response: str = str(message.clean_content)
-        if question.valid_regex is not None:
-            match = regex.fullmatch(r'{}'.format(question.valid_regex), response)
-            if match is None:
-                await message.reply(
-                    question.rejection_response + '\nPlease answer again.')  # An error message for failing the regex test, configurable per-question
-                return
+        try:
+            self.script.receive_response(str(message.clean_content))
+        except ResponseError as e:
+            await self.chat_member.send(str(e))
 
-        self.responses[question.name] = response
-
-        if self.last_asked_question + 1 >= self.script.length:
-            await self.review()
-        else:
-            await self.ask_question(self.last_asked_question + 1)
+        await self.ask_question()
 
     async def review(self):
         self.reviewing = True
@@ -191,7 +240,7 @@ class ChatBot:
 class ChatBotManager(commands.Cog):
     bot: Bot
     active_chatbots: Dict[int, ChatBot] = {}
-    loaded_scripts: Dict[str, ChatBotScript] = {}
+    loaded_scripts: Dict[str, ScriptData] = {}
 
     def __init__(self, bot: Bot):
         print('Started ChatBotManager')
@@ -202,7 +251,7 @@ class ChatBotManager(commands.Cog):
         file.close()
 
         for kind, script in scripts_data.items():
-            self.loaded_scripts[kind] = (ChatBotScript.build(kind, script))
+            self.loaded_scripts[kind] = (ScriptData.build(kind, script))
 
         log.info('ChatBotManager Initialized')
 
@@ -213,18 +262,15 @@ class ChatBotManager(commands.Cog):
             target_member: discord.Member = None
     ):
         existing = self.active_chatbots.get(chat_member.id)
-        if existing is not None:
-            existing = existing.kind
 
-        new_script_instance = copy.deepcopy(self.loaded_scripts[chatbot_kind])
         self.active_chatbots[chat_member.id] = ChatBot(
-            new_script_instance,
+            Script(self.loaded_scripts[chatbot_kind]),
             self.bot.db,
             chat_member,
             target_member
 
         )
-        await self.active_chatbots[chat_member.id].start(existing)
+        await self.active_chatbots[chat_member.id].ask_question(existing, first=True)
 
     @slash_command(guild_ids=guild_id_list)
     async def chatbots(self, ctx):
