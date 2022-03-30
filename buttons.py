0090ffb0272ff8344cb040f4c529372f8f88cdd0
Conner-Anderson <connerhanderson@gmail.com>
2022-03-30 15:31:53 -0500

    Allow up to 5 buttons in /post, Added radon
    Update poetry.lock

--- buttons.py
+++ buttons.py
@@ -103,25 +103,39 @@ class HVZButton(discord.ui.Button):
 
 async def post(
         ctx: discord.ApplicationContext,
-        function_name: str,
-        message: str = ''
+        button_1: str,
+        text: str = '',
+        button_2: str = None,
+        button_3: str = None,
+        button_4: str = None,
+        button_5: str = None
 ):
-    """Post a message with a button: registration or tag log. Can change message."""
 
     # timeout is None because we want this view to be persistent
+    input_functions = [button_1, button_2, button_3, button_4, button_5]
     view = discord.ui.View(timeout=None)
     postable_buttons = ctx.bot.get_cog('HVZButtonCog').postable_buttons
-
-    for b in postable_buttons:
-        if b.custom_id == function_name:
-            view.add_item(b)
-            break
+    error_msg = ''
+    added_functions = []
+    for f in input_functions:
+        if not f:
+            continue
+        if f in added_functions:
+            error_msg += f'The button "{f}" is already added. Cannot use the same button twice in one post.'
+            continue
+        for b in postable_buttons:
+            if b.custom_id == f:
+                view.add_item(b)
+                added_functions.append(f)
+                break
+        else:
+            error_msg += f'Could not find the postable button "{f}". Probably a bug.\n'
+    if added_functions:
+        await ctx.channel.send(text, view=view)
+    if error_msg:
+        await ctx.respond(error_msg, ephemeral=True)
     else:
-        await ctx.respond(f'Could not find the postable button "{function_name}". Probably a bug.')
-        return
-
-    await ctx.channel.send(message, view=view)
-    await ctx.respond('Posted message with button.', ephemeral=True)
+        await ctx.respond('Posted message with button.', ephemeral=True)
 
 
 class HVZButtonCog(commands.Cog):
@@ -147,9 +161,14 @@ class HVZButtonCog(commands.Cog):
             func=post,
             guild_ids=guild_id_list,
             options=[
-                Option(str, 'Which button to post.', choices=button_options, name='function_name', required=True),
-                Option(str, 'Text of the message.', name='text', required=False, default='')
-            ]
+                Option(str, 'Button to post.', choices=button_options, name='button_1', required=True),
+                Option(str, 'Optional: Text of the message.', name='text', required=False, default=''),
+                Option(str, 'Optional: Second button.', choices=button_options, name='button_2', required=False, default=None),
+                Option(str, 'Optional: Third button.', choices=button_options, name='button_3', required=False, default=None),
+                Option(str, 'Optional: Fourth button.', choices=button_options, name='button_4', required=False, default=None),
+                Option(str, 'Optional: Fifth button.', choices=button_options, name='button_5', required=False, default=None)
+            ],
+            description='Posts a message with buttons that will launch chatbots. Buttons persist and can exist multiply.'
         )
         self.bot.add_application_command(command)
         await self.bot.sync_commands(guild_ids=guild_id_list, register_guild_commands=True)
