1cef2508afb007270e5ceeb0629687134e4c7883
Conner-Anderson <connerhanderson@gmail.com>
2023-08-11 15:46:37 -0500

    Preliminary, non-functional config test

--- discord_hvz/config.py
+++ discord_hvz/config.py
@@ -1,16 +1,20 @@
 from __future__ import annotations
 
 import sys
-from typing import Dict, List, Any
+import zoneinfo
+from typing import Dict, List, Any, Union
+
+import pydantic
 from ruamel.yaml import YAML
 from datetime import datetime, timedelta, timezone, tzinfo
 from zoneinfo import ZoneInfo
 from dateutil import tz
 from pathlib import Path
-from pydantic import BaseModel, validator, BeforeValidator, ConfigDict, SkipValidation
+from pydantic import BaseModel, BeforeValidator, AfterValidator, PlainValidator, ValidationError, Field
+from pydantic_core import ErrorDetails
 from pydantic_yaml import parse_yaml_raw_as, to_yaml_str
 
-from dataclasses import dataclass, Field
+from dataclasses import dataclass
 from typing_extensions import Annotated
 
 from loguru import logger
@@ -24,35 +28,102 @@ yaml.preserve_quotes = True
 
 DEFAULT_DB_PATH = Path('game_database.db')
 
+CUSTOM_MESSAGES = {
+    'value_error': "{formatted_loc} set to '{input}': {msg}",
+    'int_parsing': "{formatted_loc} set to '{input}': This must be an integer.",
+    'bool_parsing': "{formatted_loc} set to '{input}': This must be a boolean value, such as True or False.",
+    'string_type': "{formatted_loc} set to '{input}': This must be text.",
+    'url_scheme': 'Hey, use the right URL scheme! I wanted {expected_schemes}.',
+}
+
+
+def convert_errors(e: ValidationError, custom_messages: Dict[str, str]) -> str:
+    '''
+    Converts a ValidationError into a string listing the errors meant for end users.
+    custom_messages is a dict mapping the error type to a message which may include formatting variables.
+    Errors types: https://docs.pydantic.dev/latest/errors/validation_errors/
+    Available formatting variables:
+    {loc} a tuple of locations of the option in the file
+    {formatted_loc} a formatted string of the option location, such as "sheet_names.members"
+    {input} the given input to the option
+    {msg} the original error message
+    {type} the error type
+    {url} Pydantic's documentation for this error type
+    '''
+    msg = ""
+
+    for error in e.errors():
+        formatted_loc = error["loc"][0]
+        for loc in error["loc"][1:]:
+            formatted_loc += f".{loc}"
+        custom_message = custom_messages.get(error['type'], None)
+        if not custom_message:
+            custom_message = "[{} set to {}] {}".format(formatted_loc, error["input"], error["msg"])
+            if error.get("url"):
+                custom_message += "\nSee " + error["url"]
+
+        ctx = error.get('ctx')
+        if ctx:
+            error = ctx | error
+        custom_message = custom_message.format(formatted_loc=formatted_loc, **error)
+
+        msg += custom_message + "\n"
+    return msg
+
 def to_tzinfo(x: Any) -> ZoneInfo:
+    """Converts any input into a ZoneInfo object. Meant as a Pydantic PlainValidator."""
     try:
-        timezone_offset = int(x)
-    except ValueError:
-        result = ZoneInfo(str(x))
-        if result is None:
-            raise ConfigError(f'The given timezone setting "{x}" is invalid. It can be an offset '
+        return ZoneInfo(str(x))
+    except Exception:
+        try:
+            shifted_datetime = datetime.now() + timedelta(hours=int(x))
+            return ZoneInfo(str(shifted_datetime.astimezone().tzinfo))
+        except Exception:
+            raise ValueError(f'The given timezone setting "{x}" is invalid. It can be an offset '
                               f'from UTC, such as "-5", or a preferably an IANA timezone database name. See '
                               f'https://en.wikipedia.org/wiki/List_of_tz_database_time_zones')
-        if tz.datetime_ambiguous(datetime.now(), tz=result):
-            logger.warning(f'The timezone ({x}) you have chosen is a real '
-                           f'timezone, but could lead to ambiguous times because of Daylight Savings or a similar '
-                           f'thing. A location is better.')
-    else:
-        result = timezone(
-            offset=timedelta(hours=int(timezone_offset))
-        )
-        shifted_datetime = datetime.now() + timedelta(hours=int(timezone_offset))
-        result = ZoneInfo(str(shifted_datetime.astimezone().tzinfo))
-        return result
 
-RealDate = Annotated[ZoneInfo, BeforeValidator(to_tzinfo), SkipValidation]
+def validate_database_type(x: str) -> str:
+    valid_types = ["string", "boolean", "incrementing_integer", "datetime", "integer"]
+    x = x.strip().lower()
+    for type in valid_types:
+        if type == x: return x
+    else: raise ValueError(f"'{x}' is not a valid database type. Must be one of: string, boolean, integer, datetime, incrementing_integer")
 
-@dataclass
+
+RealDate = Annotated[ZoneInfo, PlainValidator(to_tzinfo)]
+DatabaseType = Annotated[str, AfterValidator(validate_database_type)]
+
+class ChannelNames(BaseModel):
+    tag_announcements: str = Field(default="tag-announcements", alias="tag-announcements")
+    report_tags: str = Field(default="report-tags", alias="report-tags")
+    zombie_chat: str = Field(default="zombie-chat", alias="zombie-chat")
+
+class RoleNames(BaseModel):
+    zombie: str = Field(default="zombie")
+    report_tags: str = Field(default="human")
+    zombie_chat: str = Field(default="player")
+
+
 class MyModel(BaseModel):
-    model_config = ConfigDict(arbitrary_types_allowed=True)
+    #model_config = ConfigDict(arbitrary_types_allowed=True)
     server_id: int
     sheet_id: str
     timezone: RealDate
+    registration: bool
+    tag_logging: bool
+    google_sheet_export: bool
+    sheet_names: Dict[str, str]
+    channel_names: ChannelNames
+    role_names: RoleNames
+    database_tables: Dict[str, Dict[str, DatabaseType]]
+    database_path: str
+
+    class Config:
+        arbitrary_types_allowed = True
+
+
+
 
 
 class ConfigError(Exception):
@@ -226,5 +297,13 @@ if __name__ == "__main__":
     path = Path().cwd().parent / "config.yml"
     with open(path) as fp:
         yml = yaml.load(fp)
-    m1 = parse_yaml_raw_as(MyModel, str(yml))
-    logger.info(f"{m1.server_id} {m1.timezone}")
+    try:
+        m1 = parse_yaml_raw_as(MyModel, str(yml))
+    except pydantic.ValidationError as e:
+        msg = f"There were errors reading the configuration: \n" + convert_errors(e, CUSTOM_MESSAGES)
+
+        logger.error(msg)
+
+
+    else:
+        logger.info(f"{m1.server_id} {m1.timezone} {m1.channel_names.tag_announcements}")
