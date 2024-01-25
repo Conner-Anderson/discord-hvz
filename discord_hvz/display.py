4e92f090bc0676ed75cac743484cf2ca5dbd15e2
Conner-Anderson <connerhanderson@gmail.com>
2024-01-25 10:54:33 -0600

    Beginning conversion to QuickChart

--- discord_hvz/display.py
+++ discord_hvz/display.py
@@ -10,6 +10,7 @@ import discord
 import pandas as pd
 import pandas.util
 import plotly.express as px
+from quickchart import QuickChart
 import sqlalchemy
 from discord.commands import slash_command, Option
 from loguru import logger
@@ -23,6 +24,7 @@ if TYPE_CHECKING:
 
 guild_id_list = [config.server_id]
 LAST_GAME_PLOT_HASH = None
+LAST_GAME_PLOT_URL = ""
 
 def create_game_plot(db: 'HvzDb', filepath=None) -> discord.File:
     global LAST_GAME_PLOT_HASH
@@ -97,6 +99,107 @@ def create_game_plot(db: 'HvzDb', filepath=None) -> discord.File:
 
     file = discord.File(image_path)
     return file
+
+def create_quickchart(filepath: Path) -> str:
+    global LAST_GAME_PLOT_HASH
+
+    image_path = "images/latest_gameplot.jpeg"
+
+    # TODO: Access the database in a more sustainable way
+    engine = sqlalchemy.create_engine(f"sqlite+pysqlite:///{str(filepath)}")
+    tags_df = pd.read_sql_table('tags', con=engine, columns=['tag_time', 'revoked_tag'])
+    new_hash = pandas.util.hash_pandas_object(tags_df).sum()
+
+    if len(tags_df.index) == 0:
+        fig = px.line(tags_df, x="tag_time", y=["Zombie_Count", "Human_Count"], title='Error: There are no tags yet', markers=True)
+        fig.write_image(image_path, width=800, height=600, scale=1.5)
+        LAST_GAME_PLOT_HASH = new_hash
+
+    members_df = pd.read_sql_table('members', con=engine, columns=['registration_time', 'oz'])
+
+    def total_players(x):
+        total = (members_df.registration_time <= x.tag_time)
+        return total.sum()
+
+    def total_zombies(x):
+        '''
+        To be given a pandas series which is a single tag. Compares the tag to the dataframe of all tags,
+        counting how many precede it (including it). Excludes revoked tags
+        '''
+        total = (tags_df.tag_time <= x.tag_time) & (tags_df.revoked_tag == False)
+        return total.sum()
+
+    def format_datapoint(timestamp, y):
+        return {
+            "x": timestamp.isoformat(),
+            "y": y
+        }
+
+    oz_count = members_df['oz'].sum()
+
+    player_count_sr = tags_df.apply(total_players, axis=1)
+    tags_df = tags_df.assign(Player_Count=player_count_sr)
+    zombie_count_sr = tags_df.apply(total_zombies, axis=1) + oz_count
+    tags_df = tags_df.assign(Zombie_Count=zombie_count_sr)
+    tags_df['Human_Count'] = tags_df['Player_Count'] - tags_df['Zombie_Count']
+    tags_df.sort_values(by='tag_time', inplace=True)
+
+
+    zombie_series = [format_datapoint(x, y) for x, y in zip(tags_df['tag_time'], tags_df['Zombie_Count'])]
+    human_series = [format_datapoint(x, y) for x, y in zip(tags_df['tag_time'], tags_df['Human_Count'])]
+    #player_series = [format_datapoint(x, y) for x, y in zip(tags_df['tag_time'], tags_df['Player_Count'])]
+
+    qc = QuickChart()
+    qc.width = 1000
+    qc.height = 600
+    data = {
+            "datasets": [
+                {
+                    "label": "Zombie Count",
+                    "fill": "false",
+                    "data": zombie_series,
+                    "borderColor": "#13ad20",
+                    "backgroundColor": "#13ad20",
+                },
+                {
+                    "label": "Human Count",
+                    "fill": "false",
+                    "data": human_series,
+                    "borderColor": "#dbce14",
+                    "backgroundColor": "#dbce14",
+                },
+            ]
+        }
+    qc.config = {
+        "type": "line",
+        "stacked": "false",
+        "data": data,
+        "options": {
+            "title": {
+                "display": "true",
+                "text": "Players over Time",
+                "fontSize": 16,
+            },
+            "scales": {
+                "xAxes": [{
+                    "type": "time",
+                    "time": {
+                        #"unit": "day",
+                        "minUnit": "hour",
+                        "displayFormats": {
+                            "hour": "ddd, H:mm",
+                            "day": "ddd, MMM DD"
+                        }
+                    }
+                }]
+            }
+        }
+    }
+
+    url = qc.get_url()
+    print(qc.get_url())
+
+    return url
 
 
 
@@ -192,10 +295,10 @@ class GamePlotElement(PanelElement):
     def refresh_event(self):
         return 'on_role_change'
 
-    def add(self, embed: discord.Embed, panel: "HVZPanel") -> discord.File:
-        file = create_game_plot(panel.bot.db)
-        embed.set_image(url=f'attachment://{file.filename}')
-        return file
+    def add(self, embed: discord.Embed, panel: "HVZPanel") -> None:
+        url = create_quickchart(panel.bot.db.filepath)
+        embed.set_image(url=url)
+        #return file
 
 
 class TagTreeElement(PanelElement):
--- discord_hvz/display_test.py
+++ discord_hvz/display_test.py
@@ -19,76 +19,8 @@ logger.add(sys.stderr, level="INFO")
 
 
 LAST_GAME_PLOT_HASH = None
+LAST_PLOT_URL = ""
 
-def create_game_plot(filepath) -> discord.File:
-    global LAST_GAME_PLOT_HASH
-
-    image_path = "images/latest_gameplot.jpeg"
-
-    # TODO: Access the database in a more sustainable way
-    engine = sqlalchemy.create_engine(f"sqlite+pysqlite:///{str(filepath)}")
-    tags_df = pd.read_sql_table('tags', con=engine, columns=['tag_time', 'revoked_tag'])
-    new_hash = pandas.util.hash_pandas_object(tags_df).sum()
-
-    if len(tags_df.index) == 0:
-        fig = px.line(tags_df, x="tag_time", y=["Zombie_Count", "Human_Count"], title='Error: There are no tags yet', markers=True)
-        fig.write_image(image_path, width=800, height=600, scale=1.5)
-        LAST_GAME_PLOT_HASH = new_hash
-
-
-    elif LAST_GAME_PLOT_HASH != new_hash or not image_path.exists():
-        members_df = pd.read_sql_table('members', con=engine, columns=['registration_time', 'oz'])
-
-        def total_players(x):
-            total = (members_df.registration_time <= x.tag_time)
-            return total.sum()
-
-        def total_zombies(x):
-            '''
-            To be given a pandas series which is a single tag. Compares the tag to the dataframe of all tags,
-            counting how many precede it (including it). Excludes revoked tags
-            '''
-            total = (tags_df.tag_time <= x.tag_time) & (tags_df.revoked_tag == False)
-            return total.sum()
-
-        oz_count = members_df['oz'].sum()
-
-        player_count_sr = tags_df.apply(total_players, axis=1)
-        tags_df = tags_df.assign(Player_Count=player_count_sr)
-        zombie_count_sr = tags_df.apply(total_zombies, axis=1) + oz_count
-        tags_df = tags_df.assign(Zombie_Count=zombie_count_sr)
-        tags_df['Human_Count'] = tags_df['Player_Count'] - tags_df['Zombie_Count']
-        tags_df.sort_values(by='tag_time', inplace=True)
-
-        title = "Players over Time"
-        fig = px.line(tags_df, x="tag_time", y=["Zombie_Count", "Human_Count"], title=title, markers=True)
-        fig.update_layout(
-            xaxis_title = 'Tag Time',
-            yaxis_title = 'Player Count',
-            legend_title = 'Plots',
-            title_xanchor = 'auto'
-        )
-        fig.update_traces(
-            patch={'line_color': '#32C744'},
-            selector={'name': 'Zombie_Count'}
-        )
-        fig.update_traces(
-            patch={'line_color': '#F1C40F'},
-            selector={'name': 'Human_Count'}
-        )
-        fig.update_xaxes(
-            dtick=3600000 * 24,  # The big number is one hour
-            tickformat="%a %b %d",
-            ticks='outside',
-            ticklabelmode='period'
-        )
-        # fig.show()
-
-        fig.write_image(image_path, width=800, height=600, scale=1.5)
-        LAST_GAME_PLOT_HASH = new_hash
-
-    file = discord.File(image_path)
-    return file
 
 def create_quickchart(filepath) -> discord.File:
     global LAST_GAME_PLOT_HASH
@@ -104,8 +36,6 @@ def create_quickchart(filepath) -> discord.File:
         fig = px.line(tags_df, x="tag_time", y=["Zombie_Count", "Human_Count"], title='Error: There are no tags yet', markers=True)
         fig.write_image(image_path, width=800, height=600, scale=1.5)
         LAST_GAME_PLOT_HASH = new_hash
-
-
 
     members_df = pd.read_sql_table('members', con=engine, columns=['registration_time', 'oz'])
 
@@ -136,13 +66,10 @@ def create_quickchart(filepath) -> discord.File:
     tags_df['Human_Count'] = tags_df['Player_Count'] - tags_df['Zombie_Count']
     tags_df.sort_values(by='tag_time', inplace=True)
 
-    title = "Players over Time"
-    #print(tags_df)
 
     zombie_series = [format_datapoint(x, y) for x, y in zip(tags_df['tag_time'], tags_df['Zombie_Count'])]
     human_series = [format_datapoint(x, y) for x, y in zip(tags_df['tag_time'], tags_df['Human_Count'])]
-    player_series = [format_datapoint(x, y) for x, y in zip(tags_df['tag_time'], tags_df['Player_Count'])]
-    print(zombie_series)
+    #player_series = [format_datapoint(x, y) for x, y in zip(tags_df['tag_time'], tags_df['Player_Count'])]
 
     qc = QuickChart()
     qc.width = 1000
@@ -150,19 +77,18 @@ def create_quickchart(filepath) -> discord.File:
     data = {
             "datasets": [
                 {
-                    "label": "Number of Zombies",
+                    "label": "Zombie Count",
                     "fill": "false",
-                    "data": zombie_series
+                    "data": zombie_series,
+                    "borderColor": "#13ad20",
+                    "backgroundColor": "#13ad20",
                 },
                 {
-                    "label": "Number of Humans",
+                    "label": "Human Count",
                     "fill": "false",
-                    "data": human_series
-                },
-                {
-                    "label": "Number of Players",
-                    "fill": "false",
-                    "data": player_series
+                    "data": human_series,
+                    "borderColor": "#dbce14",
+                    "backgroundColor": "#dbce14",
                 },
             ]
         }
@@ -171,12 +97,19 @@ def create_quickchart(filepath) -> discord.File:
         "stacked": "false",
         "data": data,
         "options": {
+            "title": {
+                "display": "true",
+                "text": "Players over Time",
+                "fontSize": 16,
+            },
             "scales": {
                 "xAxes": [{
                     "type": "time",
                     "time": {
-                        "unit": "day",
+                        #"unit": "day",
+                        "minUnit": "hour",
                         "displayFormats": {
+                            "hour": "ddd, H:mm",
                             "day": "ddd, MMM DD"
                         }
                     }
@@ -185,50 +118,7 @@ def create_quickchart(filepath) -> discord.File:
         }
     }
 
-    boo = {
-      "type": "line",
-      "data": {
-        "datasets": [
-          {
-            "label": "Time series example",
-            "fill": "false",
-            "data": [
-              {
-                "x": "1695647820.0",
-                "y": -29
-              },
-              {
-                "x": "1695652740.0",
-                "y": -34
-              },
-              {
-                "x": "1695658140.0",
-                "y": -62
-              },
-              {
-                "x": "1695676620.0",
-                "y": 1
-              }
-            ]
-          }
-        ]
-      },
-      "options": {
-        "scales": {
-          "xAxes": [{
-            "type": "time",
-            "time": {
-              "parser": "x",
-              "displayFormats": {
-                "day": "MMM DD"
-              }
-            }
-          }]
-        }
-      }
-    }
-
-
+    url = qc.get_url()
     print(qc.get_url())
 
     file = discord.File(image_path)
@@ -259,5 +149,5 @@ New players today: On registration
 if __name__ == '__main__':
 
     print("Trying")
-    create_quickchart('game_database.db')
+    create_quickchart('hvzdb.db')
 
